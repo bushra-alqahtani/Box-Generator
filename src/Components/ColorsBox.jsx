6c2e98f0d234fecc6c20fa03cc72a326968e9431
Bushra AlQahtani <89680878+bushra-alqahtani@users.noreply.github.com>
2022-08-21 20:19:40 +0300

    Create ColorsBox.jsx

---
+++ src/Components/ColorsBox.jsx
@@ -0,0 +1,11 @@
+// this is another way to generate a box
+// rest of code i put it in App.js
+
+function ColorsBox({color}){
+return(
+    <div style={{margin:"3px",width:"100px" ,height:"100px", backgroundColor:color }}></div>
+)
+
+}
+
+export default ColorsBox;
