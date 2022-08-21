b61057b352f291c9f7e176b503052d525f77320a
Bushra AlQahtani <89680878+bushra-alqahtani@users.noreply.github.com>
2022-08-21 21:14:47 +0300

    i add hooks

--- src/App.js
+++ src/App.js
@@ -2,26 +2,26 @@ import './App.css';
 import React, { useState } from 'react';
 //import MessageForm from './Components/MessageForm';
 //import MessageDisplay from './Components/MessageDisplay';
-import ColorsBox from './Components/ColorsBox'
+import ColorsBox from './Components/ColorsBox';
+import {InputHook} from './Hooks/InputHook';
 
 function App() {
-const [color,setColor]=useState("");
-const [colorArray,setColorArray]=useState([]);
-
-function handelColors(e){
-  setColor(e.target.value);
-}
+  const [colorArray,setColorArray]=useState([]);
+  const color=InputHook();
 function addColors(e){
-setColorArray([...colorArray,color])
+setColorArray([...colorArray,color.value])
+}
+function handelRemove(color){
+  setColorArray(colorArray.filter(c=> color !== c))
 }
 
   return (
       <div className='App'>
         <span>enter color</span>
-        <input value={color} onChange={handelColors} type="text" name="color"/>&nbsp;
+        <input { ...color }/>&nbsp;
         <button onClick={addColors}>Create a box</button>
         <div style={{display:"flex", justifyContent:"center"}}>
-          {colorArray.map((color,index)=> <ColorsBox color={color}/>)}
+          {colorArray.map((color,index)=> <ColorsBox color={color} handelRemove={()=>handelRemove(color)}/>)}
        
         </div>
          
