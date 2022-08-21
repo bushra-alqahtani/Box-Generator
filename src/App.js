9d9df0fab7363326bb80391a598e7c78fef2b384
Bushra AlQahtani <89680878+bushra-alqahtani@users.noreply.github.com>
2022-08-21 20:20:07 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import './App.css';
 import React, { useState } from 'react';
-import MessageForm from './Components/MessageForm';
+//import MessageForm from './Components/MessageForm';
 //import MessageDisplay from './Components/MessageDisplay';
-
+import ColorsBox from './Components/ColorsBox'
 
 function App() {
+const [color,setColor]=useState("");
+const [colorArray,setColorArray]=useState([]);
+
+function handelColors(e){
+  setColor(e.target.value);
+}
+function addColors(e){
+setColorArray([...colorArray,color])
+}
 
   return (
-      <>
-          <MessageForm />
+      <div className='App'>
+        <span>enter color</span>
+        <input value={color} onChange={handelColors} type="text" name="color"/>&nbsp;
+        <button onClick={addColors}>Create a box</button>
+        <div style={{display:"flex", justifyContent:"center"}}>
+          {colorArray.map((color,index)=> <ColorsBox color={color}/>)}
+       
+        </div>
+         
           
-      </>
+      </div>
   );
 }
 
